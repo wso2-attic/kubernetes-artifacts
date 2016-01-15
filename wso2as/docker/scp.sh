1a8ca97e7b6c247acd8592c9fad3a06394748378
Isuru Haththotuwa <isuruh@apache.org>
2016-01-15 08:14:19 +0530

    correcting call to scp-cmd.sh in wso2as/docker/scp.sh

--- wso2as/docker/scp.sh
+++ wso2as/docker/scp.sh
@@ -20,7 +20,7 @@ set -e
 
 product_name=as
 product_version=5.3.0
-minions='core@ip1|core@ip2'
+minions='core@nodeip-1|core@nodeip-2'
 image_version=$1
 product_profiles=$2
 
@@ -41,8 +41,8 @@ prgdir=`dirname "$0"`
 script_path=`cd "$prgdir"; pwd`
 common_folder=`cd "${script_path}/../../common/scripts/docker/"; pwd`
 
-echo "Importing docker images to master and minion-1"
-bash ${common_folder}/scp-cmd.sh ${product_name} ${product_version} ${image_version} ${product_version} ${minions}
+echo "Importing docker images to minions"
+bash ${common_folder}/scp-cmd.sh ${product_name} ${product_version} ${image_version} ${product_profiles} ${minions}
 pid1=$!
 
 wait $pid1
