fdcaa992074949282ba5488c73a5c4ea31386791
Imesh Gunaratne <imesh@wso2.com>
2016-02-18 23:31:43 +0530

    Adding a parameter to API-M deploy.sh to select the deployment pattern

--- wso2am/kubernetes/deploy.sh
+++ wso2am/kubernetes/deploy.sh
@@ -24,76 +24,95 @@ publisher_port=32012
 store_port=32014
 gateway_port=32007
 
-#echo "Deploying wso2am default service..."
-#kubectl create -f wso2am-default-service.yaml
+# Deploy using default profile
+function default {
+    echo "Deploying wso2am default service..."
+    kubectl create -f wso2am-default-service.yaml
 
-# deploy all services
+    echo "Deploying wso2am default controller..."
+    kubectl create -f wso2am-default-controller.yaml
 
-echo "Deploying wso2am key manager service..."
-kubectl create -f wso2am-key-manager-service.yaml
+    echo "Waiting wso2am to launch on http://${host}:${default_port}"
+    until $(curl --output /dev/null --silent --head --fail http://${host}:${default_port}); do
+        printf '.'
+        sleep 5
+    done
 
-echo "Deploying wso2am store service..."
-kubectl create -f wso2am-store-service.yaml
+    echo -e "\nwso2am launched!"
+}
 
-echo "Deploying wso2am publisher service..."
-kubectl create -f wso2am-publisher-service.yaml
+# Deploy using separate profiles
+function distributed {
+    echo "Deploying wso2am key manager service..."
+    kubectl create -f wso2am-key-manager-service.yaml
 
-echo "Deploying wso2am gateway manager service..."
-kubectl create -f wso2am-gateway-service.yaml
+    echo "Deploying wso2am store service..."
+    kubectl create -f wso2am-store-service.yaml
 
-# deploy the controllers
+    echo "Deploying wso2am publisher service..."
+    kubectl create -f wso2am-publisher-service.yaml
 
-echo "Deploying wso2am key manager controller..."
-kubectl create -f wso2am-key-manager-controller.yaml
+    echo "Deploying wso2am gateway manager service..."
+    kubectl create -f wso2am-gateway-service.yaml
 
-echo "Waiting wso2am key manager to launch on http://${host}:${km_port}"
-until $(curl --output /dev/null --silent --head --fail http://${host}:${km_port}); do
-    printf '.'
-    sleep 5
-done
+    # deploy the controllers
 
-echo -e "\nwso2am key manager launched!"
+    echo "Deploying wso2am key manager controller..."
+    kubectl create -f wso2am-key-manager-controller.yaml
 
-#echo "Deploying wso2am default controller..."
-#kubectl create -f wso2am-default-controller.yaml
-#
-#echo "Waiting wso2am to launch on http://${host}:${default_port}"
-#until $(curl --output /dev/null --silent --head --fail http://${host}:${default_port}); do
-#    printf '.'
-#    sleep 5
-#done
+    echo "Waiting wso2am key manager to launch on http://${host}:${km_port}"
+    until $(curl --output /dev/null --silent --head --fail http://${host}:${km_port}); do
+        printf '.'
+        sleep 5
+    done
 
-#echo -e "\nwso2am launched!"
+    echo -e "\nwso2am key manager launched!"
 
-echo "Deploying wso2am store controller..."
-kubectl create -f wso2am-store-controller.yaml
+    echo "Deploying wso2am store controller..."
+    kubectl create -f wso2am-store-controller.yaml
 
-echo "Waiting wso2am store to launch on http://${host}:${store_port}"
-until $(curl --output /dev/null --silent --head --fail http://${host}:${store_port}); do
-    printf '.'
-    sleep 5
-done
+    echo "Waiting wso2am store to launch on http://${host}:${store_port}"
+    until $(curl --output /dev/null --silent --head --fail http://${host}:${store_port}); do
+        printf '.'
+        sleep 5
+    done
 
-echo -e "\nwso2am store launched!"
+    echo -e "\nwso2am store launched!"
 
-echo "Deploying wso2am publisher controller..."
-kubectl create -f wso2am-publisher-controller.yaml
+    echo "Deploying wso2am publisher controller..."
+    kubectl create -f wso2am-publisher-controller.yaml
 
-echo "Waiting wso2am publisher to launch on http://${host}:${publisher_port}"
-until $(curl --output /dev/null --silent --head --fail http://${host}:${publisher_port}); do
-    printf '.'
-    sleep 5
-done
+    echo "Waiting wso2am publisher to launch on http://${host}:${publisher_port}"
+    until $(curl --output /dev/null --silent --head --fail http://${host}:${publisher_port}); do
+        printf '.'
+        sleep 5
+    done
 
-echo -e "\nwso2am publisher launched!"
+    echo -e "\nwso2am publisher launched!"
 
-echo "Deploying wso2am gateway manager controller..."
-kubectl create -f wso2am-gateway-controller.yaml
+    echo "Deploying wso2am gateway manager controller..."
+    kubectl create -f wso2am-gateway-controller.yaml
 
-echo "Waiting wso2am gateway manager to launch on http://${host}:${gateway_port}"
-until $(curl --output /dev/null --silent --head --fail http://${host}:${gateway_port}); do
-    printf '.'
-    sleep 5
-done
+    echo "Waiting wso2am gateway manager to launch on http://${host}:${gateway_port}"
+    until $(curl --output /dev/null --silent --head --fail http://${host}:${gateway_port}); do
+        printf '.'
+        sleep 5
+    done
 
-echo -e "\nwso2am gateway manager launched!"
+    echo -e "\nwso2am gateway manager launched!"
+}
+
+pattern=$1
+if [ -z "$pattern" ]
+  then
+    pattern='default'
+fi
+
+if [ "$pattern" = "default" ]; then
+  default
+elif [ "$pattern" = "distributed" ]; then
+  distributed
+else
+  echo "Usage: ./deploy.sh [default|distributed]"
+  exit
+fi
